d7e7e0e5a453c0101109d9b3772da91b0a0971e5
Himanshu Abburu <himanshuabburu@gmail.com>
2022-02-17 00:20:04 +0000

    Update to add responsive navbar

--- page2.js
+++ page2.js
@@ -1,3 +1,19 @@
+const menu = document.querySelector(".menuToggler");
+const drop = document.querySelector(".dropdown");
+let menuopen = false;
+menu.addEventListener("click", () => {
+  console.log("click");
+  if (!menuopen) {
+    menu.classList.add("open");
+    drop.classList.add("open");
+    menuopen = true;
+  } else {
+    menu.classList.remove("open");
+    drop.classList.remove("open");
+    menuopen = false;
+  }
+});
+
 const quizQuestions = [
   {
     question: "Who played the role of Iron Man?",
