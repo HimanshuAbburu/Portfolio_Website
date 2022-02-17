7116a7fd00a94459d0ecf229ef65dec0d1d7a138
Himanshu Abburu <himanshuabburu@gmail.com>
2022-02-17 00:19:32 +0000

    Update to add responsive navbar

---
+++ page4.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector(".menuToggler");
+const drop = document.querySelector(".dropdown");
+let menuopen = false;
+menu.addEventListener("click", () => {
+  console.log("click");
+  if (!menuopen) {
+    menu.classList.add("open");
+    drop.classList.add("open");
+    menuopen = true;
+  } else {
+    menu.classList.remove("open");
+    drop.classList.remove("open");
+    menuopen = false;
+  }
+});
