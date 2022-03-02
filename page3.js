414b396926fe91ff2d3ec01449effb4d62d6102b
Himanshu Abburu <himanshuabburu@gmail.com>
2022-03-02 22:07:17 +0000

    Functionality update

--- page3.js
+++ page3.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector(".menuToggler");
 const drop = document.querySelector(".dropdown");
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosList = document.getElementById("todos");
+
 let menuopen = false;
+
 menu.addEventListener("click", () => {
   console.log("click");
   if (!menuopen) {
@@ -13,3 +19,65 @@ menu.addEventListener("click", () => {
     menuopen = false;
   }
 });
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoLi = document.createElement("li");
+    if (todo && todo.completed) {
+      todoLi.classList.add("completed");
+    }
+
+    todoLi.innerText = todoText;
+
+    todoLi.addEventListener("click", () => {
+      todoLi.classList.toggle("completed");
+      updateLS();
+    });
+
+    todoLi.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoLi.remove();
+      updateLS();
+    });
+
+    todosList.appendChild(todoLi);
+
+    input.value = "";
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  todoLi = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todoLi.forEach((todoLi) => {
+    todos.push({
+      text: todoLi.innerText,
+      completed: todoLi.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
